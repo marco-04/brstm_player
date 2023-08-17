79f72eaca6704dc13d00c1cf85cd6ed2e416f6b1
marcosti <marcosti096@duck.com>
2023-08-17 17:40:06 +0200

    First commit

---
+++ bin/brstm_player.dart
@@ -0,0 +1,5 @@
+import 'package:brstm_player/brstm_player.dart' as brstm_player;
+
+void main(List<String> arguments) {
+  print('Hello world: ${brstm_player.calculate()}!');
+}
---
+++ test/brstm_player_test.dart
@@ -0,0 +1,8 @@
+import 'package:brstm_player/brstm_player.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

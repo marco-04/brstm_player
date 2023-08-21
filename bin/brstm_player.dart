dfac548980fc0e95b6e829ab1d349a5177bc6672
marcosti <marcosti096@duck.com>
2023-08-21 22:13:23 +0200

    modified: added option to use brstm_converter

--- bin/brstm_player.dart
+++ bin/brstm_player.dart
@@ -1,5 +1,5 @@
 import 'package:brstm_player/brstm.dart';
-//import 'package:brstm_player/ffmpeg_convert.dart';
+import 'package:brstm_player/ffmpeg_convert.dart';
 
 void main(List<String> arguments) async {
   var test = BRSTM("./assets/epic_sax.brstm");
@@ -12,4 +12,8 @@ void main(List<String> arguments) async {
   // test.readHeadSync();
   print(r"\\\\\\\\\");
   print(test);
+
+  convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".");
+  convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".", brstm_converter: false);
+  convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".", brstm_converter: true);
 }
--- lib/ffmpeg_convert.dart
+++ lib/ffmpeg_convert.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-Future<void> convert(String path, int codec, int tracks, int channels, String dest) async {
+Future<void> convert(String path, int codec, int tracks, int channels, String dest, {bool brstm_converter = false}) async {
   if ((channels/tracks) != 2 && (channels/tracks) != 1) {
     throw Exception("[FAILED]: Uneven channels($channels)/track($tracks) ratio");
   }
@@ -9,27 +9,100 @@ Future<void> convert(String path, int codec, int tracks, int channels, String de
   final String dest_file = "${path.substring(path.lastIndexOf("/")+1, path.lastIndexOf("."))}.wav";
   //final String ffmpeg_convert = "ffmpeg -y -i '$path' -c:a pcm_s32le '$dest$dest_file'";
   //final String ffmpeg_split_s = "ffmpeg -y -i '$dest$dest_file' -af 'pan=${channels}c|";
+  ProcessResult? process;
   
   assert(File(path).existsSync());
   
-  var process = await Process.run(
-    "ffmpeg",
-    [
-      "-y",
-      "-i",
-      "$path",
-      "-c:a",
-      "pcm_s32le",
-      "$dest/$dest_file"
-    ]
-  );
+  if (!brstm_converter) {
+    process = await Process.run(
+      "ffmpeg",
+      [
+        "-y",
+        "-i",
+        "$path",
+        "-c:a",
+        "pcm_s16le",
+        "$dest/$dest_file"
+      ]
+    );
+  } else {
+    process = await Process.run(
+      "brstm_converter",
+      [
+        "$path",
+        "-o",
+        "$dest/$dest_file"
+      ]
+    );
+  }
+  
+  print(process.stdout);
+  print(process.stderr);
+
+  if (tracks > 1) {
+    for (int i = 0; i < tracks; i++) {
+      process = await Process.run(
+        "ffmpeg",
+        [
+          "-y",
+          "-i",
+          "$dest/$dest_file",
+          "-af",
+          "pan=${channels}c|c0=c${i*2}|c1=c${(i*2)+1}",
+          "-ac",
+          "2",
+          "$dest/T$i-$dest_file"
+        ]
+      );
+      
+      print(process.stdout);
+      print(process.stderr);
+    }
+  }
+}
+
+void convertSync(String path, int codec, int tracks, int channels, String dest, {bool brstm_converter = false}) {
+  if ((channels/tracks) != 2 && (channels/tracks) != 1) {
+    throw Exception("[FAILED]: Uneven channels($channels)/track($tracks) ratio");
+  }
+  
+  // TO CHANGE ON WINDOWS
+  final String dest_file = "${path.substring(path.lastIndexOf("/")+1, path.lastIndexOf("."))}.wav";
+  //final String ffmpeg_convert = "ffmpeg -y -i '$path' -c:a pcm_s32le '$dest$dest_file'";
+  //final String ffmpeg_split_s = "ffmpeg -y -i '$dest$dest_file' -af 'pan=${channels}c|";
+  ProcessResult? process;
+  
+  assert(File(path).existsSync());
+  
+  if (!brstm_converter) {
+    process = Process.runSync(
+      "ffmpeg",
+      [
+        "-y",
+        "-i",
+        "$path",
+        "-c:a",
+        "pcm_s16le",
+        "$dest/$dest_file"
+      ]
+    );
+  } else {
+    process = Process.runSync(
+      "brstm_converter",
+      [
+        "$path",
+        "-o",
+        "$dest/$dest_file"
+      ]
+    );
+  }
   
   print(process.stdout);
   print(process.stderr);
   
   if (tracks > 1) {
     for (int i = 0; i < tracks; i++) {
-      var process = await Process.run(
+      process = Process.runSync(
         "ffmpeg",
         [
           "-y",

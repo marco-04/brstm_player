a3a7515bbc8b7999ea77fa4cc99c99bfd5188bac
marcosti <marcosti096@duck.com>
2023-08-24 16:57:13 +0200

    i discovered how tf the mpv socket works

--- bin/brstm_player.dart
+++ bin/brstm_player.dart
@@ -1,5 +1,6 @@
 import 'package:brstm_player/brstm.dart';
 import 'package:brstm_player/ffmpeg_convert.dart';
+import 'package:brstm_player/brstm_player.dart';
 
 void main(List<String> arguments) async {
   var test = BRSTM("./assets/epic_sax.brstm");
@@ -13,7 +14,10 @@ void main(List<String> arguments) async {
   print(r"\\\\\\\\\");
   print(test);
 
-  convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".");
-  convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".", brstm_converter: false);
-  convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".", brstm_converter: true);
+  // convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".");
+  // convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".", brstm_converter: false);
+  //convertSync("./assets/epic_sax.brstm", 0, 1, 2, ".", brstm_converter: true);
+
+  brstmPlayer test2 = brstmPlayer(1, 2, "./epic_sax.wav");
+  await test2.play();
 }
--- lib/brstm_player.dart
+++ lib/brstm_player.dart
@@ -1,3 +1,40 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'package:mpv_dart/mpv_dart.dart';
+
+enum BrstmPlayerSignals { getDuration, getCurrentPos, setCurrentPos, getTrack, setTrack, backwards, forwards, toggle, pause, play }
+
+class brstmPlayer {
+  int tracks = 0;
+  int channels = 0;
+  String name = "";
+  String path = "";
+
+  MPVPlayer player = MPVPlayer(verbose: true);
+
+  brstmPlayer(int tracks, int channels, String path) {
+    this.tracks = tracks;
+    this.channels = channels;
+    this.path = path.substring(0, path.lastIndexOf("/"));
+    this.name = path.substring(path.lastIndexOf("/")+1, path.length);
+
+    print("${this.path}/${this.name}");
+  }
+
+  Future<void> play() async {
+    await player.start();
+    await player.load("$path/$name");
+    sleep(const Duration(seconds: 2));
+    await player.goToPosition(30.toDouble());
+    await player.command("af", ["set", "lavfi=[pan=4c|c0=c2|c1=c3]"]);
+    sleep(const Duration(seconds: 5));
+    await player.command("af", ["set", "lavfi=[pan=4c|c0=c0|c1=c1]"]);
+    sleep(const Duration(seconds: 5));
+    await player.command("af", ["set", "lavfi=[pan=4c|c0=c2|c1=c3]"]);
+    sleep(const Duration(seconds: 5));
+    await player.quit();
+  }
+
+  Future<double> getDuration() async => await player.getDuration();
+  Future<double> getCurrentPos() async => await player.getTimePosition();
+  Future<int> getTrack() async => tracks;
 }

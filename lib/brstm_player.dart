1f2ad177af5c96f7b36a21b580e28e66f2537375
marcosti <marcosti096@duck.com>
2023-09-11 12:31:04 +0200

    WIP added timeout check, track switch, time position percent

--- lib/brstm_player.dart
+++ lib/brstm_player.dart
@@ -9,44 +9,124 @@ class MPVPlayer {
   String binary = "";
   String file = "";
   String pipe = "";
-  double seconds = 0;
+  double duration = 0;
+  double timePos = 0;
   double loopPoint = 0;
   int curTrack = 0;
+  int nTracks = 0;
   bool loop = false;
+
+  int updateInterval = 0;
 
   bool isRunning = false;
 
-  double timePos = 0;
-  
-  // List<String> cmdBuf = List<String>(10);
-  
+  bool _pingLock = false;
+  bool _pingResult = false;
+
   Process? mpvProcess;
+
+  Timer? timeUpdate;
   
   Future<void> start() async {
-    if (!await File(pipe).exists()) {
-      mpvProcess = await Process.start(binary, [file, "--input-ipc-server=$pipe", "--quiet" ], runInShell: false);
+    if (isRunning) return;
 
-      isRunning = true;
+    try {
+      mpvProcess = await Process.start(binary, [file, "--input-ipc-server=$pipe", "--quiet", "--idle=yes" ], runInShell: false);
+    } catch(e) {
+      throw Exception("[FAILED]: Cannot start mpv player instance ($e)");
+    }
 
-      mpvProcess!.stdout.transform(utf8.decoder).listen((data) {
-        print(data);
-      });
-      mpvProcess!.stderr.transform(utf8.decoder).listen((data) {
-        read(data.strip());
-        // print(data.strip());
-      });
+    isRunning = true;
 
-      Timer timeUpdate = Timer.periodic(Duration(milliseconds: 300), (timer) {
-        updateTimePos();
-      });
+    mpvProcess!.stdout.transform(utf8.decoder).listen((data) {
+      print(data);
+    });
+    mpvProcess!.stderr.transform(utf8.decoder).listen((data) {
+      read(data.strip());
+      // print(data.strip());
+    });
+
+    await setTimeUpdate();
+
+    await updateDuration();
+  }
+
+  Future<void> setTimeUpdate() async {
+    if (!isRunning) {
+      return;
+    }
+
+    timeUpdate = Timer.periodic(Duration(milliseconds: updateInterval), (timer) {
+      updateTimePos();
+    });
+  }
+
+  Future<void> cancelTimeUpdate() async {
+    if (timeUpdate != null) {
+      timeUpdate!.cancel();
+      timeUpdate = null;
     }
   }
 
-  String jsonGen(String property, String requestType) => r'{"result":${' + property + '},"requestType":"$requestType"}';
+  String jsonGen(String property, String requestType) => '{"result":$property,"requestType":"$requestType"}';
+
+  Future<void> checkIfRunning() async {
+    if (!isRunning) {
+      return;
+    }
+
+    while (_pingLock);
+
+    if (!isRunning) {
+      return;
+    }
+
+    _pingLock = true;
+
+    await send("show-text ${jsonGen("true", "ping")}");
+    await Future.delayed(Duration(milliseconds: 2*updateInterval));
+
+    isRunning = _pingResult;
+
+    if (!isRunning) {
+      cancelTimeUpdate();
+      _pingLock = false;
+      throw Exception("[FAILED]: Player reply timeout");
+    }
+
+    _pingLock = false;
+  }
+
+  Future<void> switchToTrack(int track) async {
+    if (nTracks < 2) return;
+    if (track < 0 || track >= _MAX_TRACKS) {
+      throw Exception("[FAILED]: Invalid track number");
+    }
+    await send("af set lavfi=[pan=${nTracks*2}c|c0=c${track*2}|c1=c${(track*2)+1}]");
+  }
+
+  Future<void> loadFile(String file) async {
+    await send("loadfile $file replace");
+  }
+
+  double getPercentDuration() {
+    if (!isRunning) {
+      throw Exception("[FAILED]: Player is not running");
+    }
+    if (duration == 0) {
+      throw Exception("[FAILED]: Invalid duration");
+    }
+    return timePos/duration;
+  }
 
   Future<void> updateTimePos() async {
     // await send(r'show-text {"result":${time-pos},"requestType":"playback"}');
-    await send("show-text ${jsonGen('=time-pos', 'playback')}");
+    await send("show-text ${jsonGen(r'${=time-pos}', 'playback')}");
+  }
+
+  Future<void> updateDuration() async {
+    // await send(r'show-text {"result":${time-pos},"requestType":"playback"}');
+    await send("show-text ${jsonGen(r'${=duration}', 'duration')}");
   }
 
   Future<void> toggleLoop() async {
@@ -73,9 +153,14 @@ class MPVPlayer {
     await send("set ab-loop-a $seconds");
     await send(r"set ab-loop-b ${=duration}");
   }
+  
+  Future<void> stop() async {
+    await send("stop");
+  }
 
   Future<void> quit() async {
     await send("quit");
+    await File(pipe).delete();
   }
   
   Future<void> send(String cmd) async {
@@ -96,8 +181,11 @@ class MPVPlayer {
         case "playback":
           exec = (double pos) => timePos = pos;
           break;
-        // case "":
-          // break;
+        case "duration":
+          exec = (double duration) => this.duration = duration;
+          break;
+        case "ping":
+          exec = (bool pingResult) => _pingResult = pingResult;
         default:
           break;
       }
@@ -110,12 +198,6 @@ class MPVPlayer {
     } on FormatException {
       // print(e);
       print(ret);
-    }
-  }
-  
-  Future<void> load(String path) async {
-    if (mpvProcess != null) {
-      await getTimePos();
     }
   }
 }
